b70c3a73a61da685bfd062a929f3c2393ae01f70
Cory Grewohl <corywalter.grewohl@calbaptist.edu>
2022-06-09 20:14:37 -0700

    main features done just have to add a few more features/fix a few bugs outlined in comment in index.js

--- index.js
+++ index.js
@@ -1,78 +1,106 @@
-let displayValue = '';
-let storedValue = '';
-let secondStoredValue = '';
+//add event listeners for calculator buttons
+
+//take input
+    //if number
+        //if no operator selected
+            //display on screen
+        //if operator selected
+            //clear screen
+            //display on screen
+    //if operator (+ - * /)
+        //if display empty
+            //do nothing
+        //if numberArray empty
+            //push number on screen to array
+            //save operation
+        //if numberArray.size == 1
+            //update operation
+        //if numberArray.size == 2
+            //evaluate operation
+            //update operation
+    //if equals
+        //evaluate
+
+/**
+ * TODO
+ * 1. Stop screen overflow
+ * 2. Round long numbers
+ * 3. Don't allow dividing by 0
+ * 4. Floating point numbers (.)
+ * 5. Backspace button
+ * 6. Keyboard support
+ * 7. Make it look nice
+ */
+
 let operation = '';
+let equalsPressed = false;
+const numberArray = [];
 
-function numberClick(input){
-    if(document.getElementById('display-inner' ).innerText.length > 12){
-        return;
-    }
+function takeInput(input){
 
-    if(operation != ''){
-        secondStoredValue = document.getElementById('display-inner').innerText;
-        updateDisplay('');
-    }
-
-    document.getElementById('display-inner').innerText += input;
-    displayValue = document.getElementById('display-inner').innerText;
-
-    
-    //console.log(displayValue);
-}
-
-function operatorClick(input){
-    // storedValue = document.getElementById('display-inner').innerText;
-
-    // if(storedValue == ''){
-    //     console.log("no numbers");
-    // } else if(operation != ''){
-    //     if(displayValue != ''){
-    //         equals();
-    //     }
-    // } else {
-    //     console.log(input);
-    //     console.log(displayValue);
-
-    //     operation = input;
-    //     displayValue = '';
-    //     updateDisplay('');
-    // }
-
-    if(document.getElementById('display-inner').innerText != ''){
-        if(storedValue != ''){
-            updateDisplay(operator(operation, storedValue, secondStoredValue));
+    //checks for number
+    if(Number.isFinite(input)){
+        //resets screen if equals was pressed before hand (new calculation)
+        if(equalsPressed == true){
+            updateDisplay('');
+            equalsPressed = false;
         }
-        
-        operation = input;
+        //this is for clicking another operation after a previous one
+        if(document.getElementById('display-inner').innerText != '' && operation != ''){
+            numberArray[0] = document.getElementById('display-inner').innerText;
+            updateDisplay('');
+        }
+        document.getElementById('display-inner').innerText += input;
     }
-        
-    
-}
 
+    //check for operator
+    if(input == 'add' || input == 'subtract' || input == 'multiply' || input == 'divide'){
+        //checks if display is empty
+        if(document.getElementById('display-inner').innerText == ''){
+            return;
+        }
+        //checks if numberArray has 0 values
+        if(numberArray.length == 0){
+            numberArray[0] = document.getElementById('display-inner').innerText;
+            operation = input;
+            updateDisplay('');
+            
+        } else if(numberArray.length == 1){ //checks if numberArray has 1 value
+                numberArray.push(document.getElementById('display-inner').innerText);
+                updateDisplay('');
+        } 
+        if(numberArray.length == 2){
+            updateDisplay(operator(operation, numberArray[0], numberArray[1]));
+            numberArray.pop();
+            numberArray.pop();
+            operation = input;
+        }
+    }
 
-
-//equals can check if there are two numbers stored, and then call operator()
-function equals() {
-    if(storedValue == '') {
-        console.log("stored value empty");
-        //updateDisplay('');
-    } else {
-        updateDisplay(operator(operation, storedValue, displayValue));
-        resetValues();
+    //check for equals sign
+    if(input == 'equals'){
+        if(numberArray.length == 0) return;
+        if(document.getElementById('display-inner').innerText == ''){
+            updateDisplay(numberArray[0]);
+        } else {
+            numberArray.push(document.getElementById('display-inner').innerText);
+            updateDisplay(operator(operation, numberArray[0], numberArray[1]));
+        }
+        numberArray.pop();
+        numberArray.pop();
+        operation = '';
+        equalsPressed = true;
     }
 }
 
 function allClear(){
+    operation = '';
+    numberArray.pop();
+    numberArray.pop();
     updateDisplay('');
-    resetValues();
 }
 
-function resetValues(){
-    displayValue = '';
-    storedValue = '';
-    secondStoredValue = '';
-    operation = '';
-}
+
 
 function updateDisplay(input) {
     document.getElementById('display-inner').innerText = input;
